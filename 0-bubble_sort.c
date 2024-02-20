1ba683c867c89301ad0e30a87e2074aa7407995d
abdulrahmanabouzied <abdulrahman.ab.mahmoud@gmail.com>
2024-02-20 14:02:55 +0200

    first 4 sorts

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,9 +13,10 @@ void bubble_sort(int *array, size_t size)
 	if (!array || !size)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	i = 0;
+	while (i < size)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
@@ -25,5 +26,6 @@ void bubble_sort(int *array, size_t size)
 				print_array(array, size);
 			}
 		}
+		i++;
 	}
 }
