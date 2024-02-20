6b44746ac3aaeddeed5cc4fd86c0ba91245b588f
abdulrahmanabouzied <abdulrahman.ab.mahmoud@gmail.com>
2024-02-20 13:59:31 +0200

    first 4 sorts

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,45 +1,29 @@
 #include "sort.h"
 
 /**
- * swap - swap two integers
- * @x: the first
- * @y: the second
-*/
-void swap(int *x, int *y)
-{
-	int tmp = *x;
-
-	*x = *y;
-	*y = tmp;
-}
-
-/**
  * bubble_sort - sort a list of objects
  * @array: list of objects
  * @size: number of objects
 */
 void bubble_sort(int *array, size_t size)
 {
-	int i, j;
-	int swapped = 0;
+	size_t i, j;
+	int temp;
 
 	if (!array || !size)
 		return;
 
-	for (i = 0; i < (int)size - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		swapped = 0;
-		for (j = 0; j < (int)size - i - 1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-		if (array[j] > array[j + 1])
+			if (array[j] > array[j + 1])
 			{
-				swapped = 1;
-				swap(&array[j], &array[j + 1]);
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 				print_array(array, size);
 			}
 		}
-
-		if (!swapped)
-			break;
 	}
 }
