ed6dbef1b73ffa01566d37462bab8b66e9dc837e
abdulrahmanabouzied <abdulrahman.ab.mahmoud@gmail.com>
2024-02-21 19:20:06 +0200

    counting sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -17,7 +17,7 @@ int getMax(int *arr, size_t size)
 		if (arr[i] > mx)
 			mx = arr[i];
 	}
-	return mx;
+	return (mx);
 }
 
 /**
@@ -28,45 +28,42 @@ int getMax(int *arr, size_t size)
 void counting_sort(int *array, size_t size)
 {
 	int k = getMax(array, size);
-	int *count = malloc((k + 1) * sizeof(k));
-	int *arr = malloc(size * sizeof(array[0]));
+	int *count;
+	int *arr;
 	size_t i;
 
+	if (!array || !size)
+		return;
+	count = malloc((k + 1) * sizeof(k));
+	arr = malloc(size * sizeof(array[0]));
 	if (!count)
 	{
 		if (arr)
 			free(arr);
 		return;
 	}
-
 	if (!arr)
 	{
 		free(count);
 		return;
 	}
-
 	for (i = 0; i < size; i++)
 	{
 		count[array[i]]++;
 	}
-
-
 	for (i = 1; (int)i <= k; i++)
 	{
 		count[i] += count[i - 1];
 	}
 	print_array(count, k + 1);
-	
 	for (i = size - 1; (int)i >= 0; i--)
 	{
 		arr[--count[array[i]]] = array[i];
 	}
-	
 	for (i = 0; i < size; i++)
 	{
 		array[i] = arr[i];
 	}
-
 	free(arr);
 	free(count);
 }
